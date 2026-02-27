b6c3a00400aa223917d1f680ae8d92a7948ee08f
Houzibe <dhouzibe1@gmail.com>
2026-02-27 23:46:01 +0100

    created a file for pandas assignment & Loaded datasets

---
+++ pandas-assignment.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+#Load datasets
+nigerian_market_prices = pd.read_csv('./datasets/nigerian_market_prices_assignment.csv')
+nigerian_market_prices = pd.read_csv('./datasets/nigerian_market_transaction_assignment.csv')
